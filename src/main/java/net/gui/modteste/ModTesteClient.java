b8d713e3d1a0ebe93385b3fc464ccd518690c51a
GuilhermeHMattioli <guilherme_mattioli@hotmail.com>
2025-03-18 19:39:37 -0300

    Initial commit

---
+++ src/main/java/net/gui/modteste/ModTesteClient.java
@@ -0,0 +1,10 @@
+package net.gui.modteste;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTesteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
